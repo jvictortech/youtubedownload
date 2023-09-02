c85cd5f58357d7a1ab4536a5f3039839a391bb43
jvictortech <joaoinformaticaatm@gmail.com>
2023-09-01 23:08:15 -0300

    Nova interface gráfica

--- YoutubeDown.py
+++ YoutubeDown.py
@@ -1,79 +1,55 @@
-from ast import Str
-from pickletools import string1
-from pydoc import text
 from tkinter import *
-from turtle import width
-from unittest import result 
-
+from tkinter import messagebox, filedialog
+import tkinter 
 from pytube import YouTube
 
+#Interface
+def Widgets():
 
-#INTERFACE
-janela = Tk()
+    #Campos de entrada
 
-janela.geometry('400x300')
-janela.configure(background='#dde')
+    entrada_1 = Label(root, text = "Link do Youtube:", bg="#E8D579")
+    entrada_1.grid(row= 1, column=0, pady=5, padx=5)
+    root.linkText= Entry(root, width=55, textvariable= link_youtube)
+    root.linkText.grid(row=1,column=1, pady=5, padx=5, columnspan=2)
 
-janela.title('Download Youtube')
+    entrada_2 = Label(root, text="Pasta de Destino:", bg="#E8D579")
+    entrada_2.grid(row=2, column=0, pady= 5, padx = 5)
+    root.destinoText = Entry(root, width=40, textvariable= caminho_baixar)
+    root.destinoText.grid(row=2, column=1, pady=5, padx=5)
 
-texto_inicial = Label(janela,text= 'Baixe Vídeos na Melhor Qualidade Direto do Youtube',background='#dde', foreground='#009')
-texto_inicial.grid(column= 0, row=0)
+    #Botões 
 
-texto_inicial2 = Label(janela, text= 'Sem anúcios, gratuito e seguro',background='#dde', foreground='#009')
-texto_inicial2.grid(column= 0, row=1)
+    procura = Button(root, text='Procurar', command=Diretorio, width=10, bg="#05E8E0")
+    procura.grid(row=2, column=2, pady=1, padx=1)
 
-texto_orientacao = Label(janela, text= 'Insira a URL do vídeo que deseja baixar aqui:',background='#dde', foreground='#009')
-texto_orientacao2 = Label(janela, text= 'Insira o caminho da pasta onde deseja salvar o video aqui:' ,background='#dde', foreground='#009')
-texto_orientacao.grid(column=0 ,row=2)
-texto_orientacao2.grid(column=0, row=4)
+    baixar = Button(root, text="Baixar", command= Iniciar, width=20, bg="#05e8e0")
+    baixar.grid(row=3, column=1, pady=3, padx=3)
 
-texto_tela = Label(janela, text= '')
-texto_tela.grid(column=0, row=7)
-
-texto_tela2 = Label(janela, text= '')
-texto_tela2.grid(column=0, row=8)
-
-texto_tela3 = Label(janela, text='')
-texto_tela3.grid(column=0, row=9)
-
-caixa_texto= Entry(janela)
-caixa_texto.grid(column=0, row=3)
-caixa_texto2= Entry(janela)
-caixa_texto2.grid(column=0, row=5)
+def Diretorio():
+    local_donwload = filedialog.askdirectory(initialdir="Escolha aonde quer salvar o vídeo")
+    caminho_baixar.set(local_donwload)
  
+def Iniciar():
 
-var = str(caixa_texto) 
+    YouTube_link = link_youtube.get()
+    pasta_salvar = caminho_baixar.get()
 
+    getVideo = YouTube(YouTube_link)
+    videoStream = getVideo.streams.first()
+    videoStream.download(pasta_salvar)
 
-def iniciar():
+    messagebox.showinfo("Video Baixado com Sucesso!!", "Seu video está salvo em:" + pasta_salvar)
 
-    yt = YouTube(caixa_texto.get())
-    
-    result = {
-        'Titulo': yt.title,
-        'Número de views': yt.views,
-        'Tamanho': yt.length,
-        'Avaliação': yt.rating,
-    }
+#Janela Principal
+root = tkinter.Tk()
+root.geometry("600x120")
+root.resizable(False, False)
+root.title("Baixar Vídeo do Youtube")
+root.config(background="#000000")
 
+link_youtube = StringVar()
+caminho_baixar = StringVar()
 
-    ys = yt.streams.get_highest_resolution()
-
-    texto1 = f'O video escolhido foi: {result}'
-    texto2 = f'Aguarde Seu vídeo está sendo baixado..'
-
-    texto_tela['text'] = texto1
-    texto_tela2['text'] = texto2
-
-
-    def baixar(): 
-        ys.download(str (caixa_texto2.get()))
-        texto3 = f'Seu Donwload foi concluído com sucesso'
-        texto_tela3['text'] = texto3
-
-    baixar()
-
-botao = Button(janela,text='DONWLOAD', command= iniciar)
-botao.grid(column=0, row=6)
-
-janela.mainloop()
+Widgets()
+root.mainloop()
